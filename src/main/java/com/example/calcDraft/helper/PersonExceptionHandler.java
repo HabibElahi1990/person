09bdaa463bf0e9f16db6c98fbd15989313a2adc2
h.elahi <habib.elahi1990@gmail.com>
2022-02-21 23:06:08 +0330

    exception Handler resolve problems

--- src/main/java/com/example/calcDraft/helper/PersonExceptionHandler.java
+++ src/main/java/com/example/calcDraft/helper/PersonExceptionHandler.java
@@ -5,21 +5,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+
 @ControllerAdvice
-public class ExceptionHandeller {
+public class PersonExceptionHandler {
 
     @ExceptionHandler(value = {Exception.class})
     public ResponseEntity<Object> exceptionHandler(Exception e) {
         Message message = new Message(e.getMessage(), HttpStatus.BAD_REQUEST);
-        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(message.toString(), HttpStatus.BAD_REQUEST);
     }
 }
 
 class Message {
-    private  String error;
-    private  HttpStatus errorCode;
+    private String error;
+    private HttpStatus errorCode;
 
-    public Message(String error, HttpStatus errorCode) {
+    Message(String error, HttpStatus errorCode) {
         this.error = error;
         this.errorCode = errorCode;
     }
@@ -27,8 +28,8 @@ class Message {
     @Override
     public String toString() {
         return "Message{" +
-                "error='" + error + '\'' +
-                ", errorCode='" + errorCode + '\'' +
-                '}';
+                "\n\terror='" + error + ',' +
+                "\n\terrorCode='" + errorCode +
+                "\n" + '}';
     }
 }
